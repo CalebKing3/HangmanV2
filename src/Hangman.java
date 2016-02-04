bade54b832a01472f0284ee1ae5cf230564585e4
Caleb King <calebking3@gmail.com>
2016-02-04 10:50:33 -0700

    Fixed hangman structure and game should be working again.

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,12 +1,17 @@
+import me.CalebKing.Game;
+import me.CalebKing.Prompter;
+
 /**
  * Created by caleb.king on 1/19/16.
  */
 public class Hangman {
-    public static void main(String[]args) {
-
-        Game game = new Game("caleb");
+    public static void main(String[] args) {
+        if(args.length == 0) {
+            System.out.println("Please enter a word");
+            System.exit(0);
+        }
+        Game game = new Game(args[0]);
         Prompter prompter = new Prompter(game);
-        prompter.displayProgress();
         prompter.play();
     }
 }
