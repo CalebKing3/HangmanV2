b9473e52e44380381db59593bd68010304bb8174
Caleb King <CalebKing3@gmail.com>
2016-02-15 14:22:44 -0700

    adding test suite, watch them fail!

---
+++ src/test/HangmanTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import me.CalebKing.Game;
+import me.CalebKing.Prompter;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HangmanTest {
+
+    @Test
+    public void testMain() throws Exception {
+        fail();
+    }
+}
